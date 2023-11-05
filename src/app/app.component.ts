0ed5802c81511a7328750d895df80c31b13c6c70
ngecu <devngecu@gmail.com>
2023-11-05 10:35:00 +0300

    1.Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:
+  `<h2>Weclome  {{person}} </h2> 
+  
+  <p>{{title}} is of length {{title.length}}
+
+  <h1>Lets convert the person name to uppercase {{person.toUpperCase()}}
+  
+
+  <h3>{{greetUser()}}</h3>
+
+  <h4>Site url is {{siteUrl}} </h4>
+  `,
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+   title = 'Dev Ngecu';
+   public siteUrl = window.location.href
+   public person = "Robinson Nyamu"
+
+   greetUser(){
+    return ` Hello ${this.person}`;
+   }
 }
