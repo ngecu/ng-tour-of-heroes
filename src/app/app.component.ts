16cbd53c898a8fbdacab3f7e0515672216efc3a3
ngecu <devngecu@gmail.com>
2023-11-05 10:54:34 +0300

    2.components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:
-  `<h2>Weclome  {{person}} </h2> 
+  templateUrl:'./app.component.html',
+  // template:
+  // `<h2>Weclome  {{person}} </h2> 
   
-  <p>{{title}} is of length {{title.length}}
+  // <p>{{title}} is of length {{title.length}}
 
-  <h1>Lets convert the person name to uppercase {{person.toUpperCase()}}
+  // <h1>Lets convert the person name to uppercase {{person.toUpperCase()}}
   
 
-  <h3>{{greetUser()}}</h3>
+  // <h3>{{greetUser()}}</h3>
 
-  <h4>Site url is {{siteUrl}} </h4>
-  `,
+  // <h4>Site url is {{siteUrl}} </h4>
+  // `,
   // templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector: 'app-test',
+
+      // <app-test></app-test>  
+
+  // selector: '.app-test',
+
+        // <div class="app-test"></div>
+  
+  // selector: '[app-test]',
+
+        // <div app-test > </div>
+
+
+  selector: 'app-test',
+
+
+  // templateUrl: './test.component.html',
+
+
+  // template:` <div>  Inlinee template </div>`,
+
+  templateUrl: './test.component.html',
+
+
+  // styleUrls: ['./test.component.scss']
+
+
+  // styles: [`
+  
+  // p{
+  //   color:red
+  // }
+  // `]
+
+  
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent {
+
+}
